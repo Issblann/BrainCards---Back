e83198b86fac2f83fff3372a6ad364b79025415c
Issblann <anaisabelpp@outlook.com>
2024-05-25 17:50:57 -0500

    first commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import bodyparser from 'body-parser';
+
+const app = express();
+
+app.use(bodyparser.json());
+
+const startServer = async () => {
+  app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
+};
+startServer();
