cf093419a0a7f5a9c229ff1729ca6f343cf98622
Ana Perez <109175830+Issblann@users.noreply.github.com>
2024-05-31 22:31:27 -0500

    Revert "03 service auth"

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,13 @@
 import express from 'express';
-import routes from './routes';
-import envs from './config/env';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import bodyparser from 'body-parser';
+
 const app = express();
-app.use(express.json());
-app.use(cookieParser());
-app.use('/', routes);
-const corsOptions = {
-  origin: envs.clientUrl,
-  optionsSuccessStatus: 200,
-  methods: 'GET, POST, PUT, DELETE',
-};
 
-app.use(cors(corsOptions));
-
-const port = envs.port || 3000;
+app.use(bodyparser.json());
 
 const startServer = async () => {
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  app.listen(3000, () => {
+    console.log('Server is running on port 3000');
   });
 };
-
 startServer();
--- src/domain/repositories/IUserRepository.ts
+++
@@ -1,8 +0,0 @@
-import User from '../entities/User';
-
-export interface IUserRepository {
-  findUserByEmail(email: string): Promise<User | null>;
-  findUserByUsername(username: string): Promise<User | null>;
-  findUserById(id: string): Promise<User | null>;
-  createUser(user: User): Promise<void>;
-}
